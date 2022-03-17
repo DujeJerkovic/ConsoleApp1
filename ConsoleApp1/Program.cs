69cf18aef7496358620b0977c4517b8ad8a4a8f9
Matea Kunac <65336475+matea0@users.noreply.github.com>
2022-03-17 18:18:09 +0100

    promjena

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello WorldDDDD!");
             Console.WriteLine("Novooo");
+            Console.WriteLine("evo ga");
         }
     }
 }
